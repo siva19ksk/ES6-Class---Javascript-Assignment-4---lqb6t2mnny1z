6e3e067e2a86a6ec1116ab86687f0015d2c558b3
siva19ksk <siva19ksk@gmail.com>
2023-11-06 10:05:37 +0530

    done

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 //Write your code here
+class API{
+    #secure=true;
+    constructor(url, method='GET')
+    {
+        this.url=url
+        this.method=method
+        this.#secure=url.startsWith('https')
+    }
+    isSecure(){
+        return this.#secure
+    }
+    updateUrl(newUrl) {
+        this.url = newUrl;
+        this.#secure = newUrl.startsWith('https');
+    }
 
-
-
-
+}
 
 //Do not remove this code
 module.exports = { API }
